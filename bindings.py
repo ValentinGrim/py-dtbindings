ca313e6f11cd45d3b38865a9aa64abc05c4f65f7
Valentin MONNOT <valentin.monnot@st.com>
2022-03-30 17:45:35 +0200

    Rework
    Git diff may seems a little bit off, i had indent problem so I remade 
    all indent
    
    BindingProps has been reworked:
    - property are now a NamedTuple (Present but NYI)
    - Internal var has been reviewed
    
    class Bindings:
    - allOf initialization has been integrated in a private method for 
    cleaner code
    - properties initialization has been also integrated in a private 
    methode + has been adapted to new BindingProps
    
    TODO:
    - Props integration (currently, only required and optional are working)
    - Public methode in class Binding to retrieve BindingProps class 
    information instead of directly accessing private var.
    - Test if all that work's ¯\_(ツ)_/¯

--- bindings.py
+++ bindings.py
@@ -6,6 +6,8 @@ import os, sys
 import yaml
 import re
 
+from typing import NamedTuple, Any
+
 props_key = ("oneOf", "anyOf", "allOf","const", "contains", "items", "enum")
 dtschema = os.path.expanduser("~/.local/lib/python3.8/site-packages/dtschema")
 
@@ -30,7 +32,7 @@ class SDTBindings:
             tmp = Binding(self._files_dict[key],self._files_dict,verbose)
             try:
                 self._compat_extractor(key,tmp.properties.prop["compatible"])
-            except KeyError:
+            except AttributeError:
                 pass
         if verbose > 2:
             print("[INFO]: Compatible dict initialized !")
@@ -84,121 +86,158 @@ class SDTBindings:
         return Binding(self._compat_dict[compatible],self._files_dict,self._verbose)
 
 class Binding:
-    def __init__(self, path, files_dict,verbose):
-        self._verbose = verbose
-        self._path = path.rsplit('/',1)[0]
-        self._files_dict = files_dict
-        self.file_name = path.rsplit('/',1)[1]
+	def __init__(self, path, files_dict,verbose):
+		self._verbose = verbose
+		self._path = path.rsplit('/',1)[0]
+		self._files_dict = files_dict
+		self.file_name = path.rsplit('/',1)[1]
 
-        global dtschema
+		global dtschema
 
-        try:
-            self._file = open(path, 'r')
-        except OSError:
-            print('[ERR ]: Cannot open', path)
-            print("[INFO]: If you didn't called this file, check that $ref is correctly set")
-            sys.exit(-1)
+		try:
+			self._file = open(path, 'r')
+		except OSError:
+			print('[ERR ]: Cannot open', path)
+			print("[INFO]: If you didn't called this file, check that $ref is correctly set")
+			sys.exit(-1)
 
-        self._content = yaml.load(self._file, Loader=yaml.FullLoader)
+		self._content = yaml.load(self._file, Loader=yaml.FullLoader)
 
-        # Loading basics information
-        self.id = self._content['$id'].replace('#','')
-        self.schema = self._content['$schema'].replace('#','')
-        self.maintainers = self._content['maintainers']
-        self.title = self._content['title']
+		# Loading basics information
+		self.id = self._content['$id'].replace('#','')
+		self.schema = self._content['$schema'].replace('#','')
+		self.maintainers = self._content['maintainers']
+		self.title = self._content['title']
 
-        # Init allOf and load $ref bindings if exist
-        self._refs = []
-        self._if = []
-        try:
-            for item in self._content['allOf']:
-                if '$ref' in item:
-                    path = ""
+		# Init allOf and load $ref bindings if exist
+		self._refs = []
+		self._if = []
 
-                    if "schemas/" in item['$ref']:
-                        #TODO:  Instead of spliting on '#', we should be able
-                        #       to handle the case where there node ref
-                        #       after this '#'. (If it make sens)
-                        path = dtschema + item['$ref'].split('#')[0]
-                    elif "../" in item['$ref']:
-                        path = self._path.rsplit('/',1)[0] + item['$ref'].replace('..','').replace('#','')
-                    else:
-                        if "/common.yaml" in item['$ref']:
-                            if not "../" in item['$ref']:
-                                path = self._path
-                                while path.rsplit('/',1)[1] != "bindings":
-                                    path = path.rsplit('/',1)[0]
-                                path += '/' + item['$ref'].split('#')[0]
-                        else:
-                            #TODO:  Same as above
-                            name = item['$ref'].split('#')[0].replace('.yaml','')
-                            name = name.rsplit('/',1)[0]
-                            path = self._files_dict[name]
+		# Initializing allOf node
+		self._init_allOf()
 
-                    if self._verbose > 2:
-                        print("[INFO]: Binding <%s> loading $ref <%s>" % (self.file_name, path))
+		self._props = BindingProps()
+		self._init_Properties()
 
-                    self._refs.append(Binding(path,self._files_dict,self._verbose))
-                if 'if' in item:
-                    self._if.append(item)
-        except KeyError:
-            if verbose > 2:
-                print("[INFO]: No node 'allOf' found for", self.file_name)
-            pass
+		try:
+			self.examples = self._content['examples']
+		except KeyError:
+			if verbose > 2:
+				print("[INFO]: No examples found for ", self.file_name)
 
-        try:
-            self._required = self._content['required']
-        except KeyError:
-            if verbose > 1:
-                print("[WARN]: No node 'required' found for ", self.file_name)
-            self._required = False
+	def _init_allOf(self):
+		try:
+			for item in self._content['allOf']:
+				if '$ref' in item:
+					path = ""
+					# If ref pointing on a dt-schema, path used is defined
+					# at top of this script and point on path where pip3
+					# installed dt-schema
+					if "schemas/" in item['$ref']:
+						#TODO:  Instead of spliting on '#', we should be able
+						#       to handle the case where there node ref
+						#       after this '#'. (If it make sens)
+						path = dtschema + item['$ref'].split('#')[0]
 
-        try:
-            self.properties = BindingProps(self._content['properties'], self._required)
-        except KeyError:
-            self.properties = BindingProps({},[])
-            if verbose:
-                print("[WARN]: No node 'properties' found for ", self.file_name)
-        # If $ref, load bindings from subclass
-        for binding in self._refs:
-            self.properties.add_from_BindingProp(binding.properties)
+					# Relative path
+					elif "../" in item['$ref']:
+						path = self._path.rsplit('/',1)[0] + item['$ref'].replace('..','').replace('#','')
+					else:
+						# There is multiple common.yaml.
+						# Some of them have relative path and can be process
+						# with the above statement, other generally are
+						# stored in other dir
+						# e.g. root_dir/dir_a/subdir_a/myfile.yaml
+						#	_______________________________|
+						#  |-> root_dir/dir_b/common.yaml
+						if "/common.yaml" in item['$ref']:
+							path = self._path
+							# This loop is used to get back to root dir
+							while path.rsplit('/',1)[1] != "bindings":
+								path = path.rsplit('/',1)[0]
+							path += '/' + item['$ref'].split('#')[0]
+						# Finaly, normal ref
+						else:
+							#TODO:  Same as above
+							name = item['$ref'].split('#')[0].replace('.yaml','')
+							name = name.rsplit('/',1)[0]
+							path = self._files_dict[name]
+
+					if self._verbose > 2:
+						print("[INFO]: Binding <%s> loading $ref <%s>" % (self.file_name, path))
+					self._refs.append(Binding(path,self._files_dict,self._verbose))
+
+				if 'if' in item:
+					self._if.append(item)
+
+		except KeyError:
+			if self._verbose > 2:
+				print("[INFO]: No node 'allOf' found for", self.file_name)
+			pass
+
+	def _init_Properties(self):
+		# Extract required node
+		try:
+			required = self._content['required']
+		except KeyError:
+			if self._verbose > 1:
+				print("[WARN]: No node 'required' found for ", self.file_name)
+			required = False
+		self._props.add_required(required)
+		# Exctract properties node
+		try:
+			properties = self._content['properties']
+		except KeyError:
+			if self._verbose:
+				print("[WARN]: No node 'properties' found for ", self.file_name)
+			properties = False
+		self._props.add_properties(properties)
+		# Add ref properties
+		for binding in self._refs:
+			self._props.add_from_BindingProp(binding._props)
 
 
-        try:
-            self.examples = self._content['examples']
-        except KeyError:
-            if verbose > 2:
-                print("[INFO]: No examples found for ", self.file_name)
+class Prop(NamedTuple):
+	name: str
+	value: Any
 
 class BindingProps:
-    def __init__(self, prop, required):
-        self.prop = prop
-        self._list = []
+	def __init__(self):
+		self._props = {}
+		self._required = []
+		self._optional = []
 
-        for key in self.prop:
-            self._list.append(key)
-        self._list.sort()
+	def add_required(self, required):
+		if not required:
+			return
 
-        self.required = []
-        self.optional = []
-        self._update(required)
+		self._required += required
+		# Remove duplicates
+		self._required = list(dict.fromkeys(self._required))
+		self._required.sort()
+		self._update()
 
-    def add_from_BindingProp(self,prop):
-        self.required += prop.required
-        self._list += prop._list
-        # Remove duplicates
-        self._list = list(dict.fromkeys(self._list))
-        # Update
-        self.prop.update(prop.prop)
-        self._update()
+	def add_properties(self, properties):
+		if not properties:
+			return
+		# Init or update optional list from properties
+		for key in properties:
+			self._optional.append(key)
+		self._optional.sort()
+		self._update()
 
+	def add_from_BindingProp(self,prop):
+		self._required += prop._required
+		# Remove duplicates
+		self._required = list(dict.fromkeys(self._required))
 
-    def _update(self,required=False):
-        if required:
-                self.required = required
-                self.optional = [item for item in self._list if item not in required]
-        else:
-            if self.required:
-                self.optional = [item for item in self._list if item not in self.required]
-            else:
-                self.optional = [item for item in self._list]
+		self._optional += prop._optional
+		# Remove duplicates
+		self._optional = list(dict.fromkeys(self._optional))
+		# Update
+		#self.prop.update(prop.prop)
+		self._update()
+
+	def _update(self):
+		if self._required:
+			self._optional = [item for item in self._optional if item not in self._required]
