464034ed80583a058969167b8da659229da543b5
ValentinGrim <vmonnot@outlook.com>
2022-05-12 15:33:30 +0200

    Doxygen update

--- docs/annotated_dup.js
+++ docs/annotated_dup.js
@@ -1,10 +1,10 @@
 var annotated_dup =
 [
     [ "bindings", null, [
+      [ "SDTBindings", "classbindings_1_1_s_d_t_bindings.html", "classbindings_1_1_s_d_t_bindings" ],
       [ "Binding", "classbindings_1_1_binding.html", "classbindings_1_1_binding" ],
-      [ "BindingProps", "classbindings_1_1_binding_props.html", "classbindings_1_1_binding_props" ],
-      [ "MainProp", "classbindings_1_1_main_prop.html", "classbindings_1_1_main_prop" ],
       [ "Prop", "classbindings_1_1_prop.html", null ],
-      [ "SDTBindings", "classbindings_1_1_s_d_t_bindings.html", "classbindings_1_1_s_d_t_bindings" ]
+      [ "MainProp", "classbindings_1_1_main_prop.html", "classbindings_1_1_main_prop" ],
+      [ "BindingProps", "classbindings_1_1_binding_props.html", "classbindings_1_1_binding_props" ]
     ] ]
 ];
